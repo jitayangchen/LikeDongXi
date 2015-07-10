be0853c00551c0b2f9606ae9362286dd502fd654
Yangchen <jitayangchen@163.com>
2015-07-10 10:54:42 +0800

    增加 喜欢和收藏

--- programmerjoke/likejoke.php
+++ programmerjoke/likejoke.php
@@ -4,9 +4,9 @@ require_once('function.php');
 $jokeId = $_GET['jokeId'];
 $userId = $_GET['userId'];
 
-function addJoke($con, $jokeId, $userId, $iscollect, $collectTime)
+function addJoke($con, $jokeId, $userId, $islike, $collectTime)
 {
-	$sql = "insert into pj_collect_and_like (joke_id, user_id, iscollect, time) values ('$jokeId', '$userId', '$iscollect', '$collectTime')";
+	$sql = "insert into pj_collect_and_like (joke_id, user_id, islike, time) values ('$jokeId', '$userId', '$islike', '$collectTime')";
 	
 	mysql_query($sql, $con);
 }
@@ -16,7 +16,7 @@ if (isset($jokeId) && isset($userId)) {
 	$collectTime = date("Y-m-d H:i:s");
 
 	addJoke($con, $jokeId, $userId, 1, $collectTime);
-	echo json_encode(array('status' => '1', 'function' => 'collectjoke.php'));
+	echo json_encode(array('status' => '1', 'function' => 'likejoke.php'));
 	mysql_close($con);
 }
 
