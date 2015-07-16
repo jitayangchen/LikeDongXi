07e14af0ab515f90ac916047112d364d98b7f1cb
Yangchen <jitayangchen@163.com>
2015-07-16 17:14:28 +0800

    add getpublishedjoke.php

---
+++ programmerjoke/getpublishedjokes.php
@@ -0,0 +1,57 @@
+<?php
+require_once('function.php');
+
+$userId = $_GET['userId'];
+$page = $_GET['page'];
+$limit = $_GET['limit'];
+
+if (!isset($page))
+{
+	$page = 1;
+}
+if (!isset($limit))
+{
+	$limit = 20;
+}
+
+$offset = ($page -1) * $limit;
+
+function getCollectJoke($con, $userId, $offset, $limit)
+{
+	$sql = "SELECT * FROM pj_jokes LEFT JOIN pj_user ON pj_jokes.user_id = pj_user.user_id WHERE pj_jokes.user_id = '$userId' ORDER BY pj_jokes.joke_id DESC LIMIT $offset, $limit";
+	
+	return mysql_query($sql, $con);
+	
+}
+
+$con = connectDB();
+
+$result = getCollectJoke($con, $userId, $offset, $limit);
+$jokesList = array();
+
+$i = 0;
+while($row = mysql_fetch_array($result))
+{
+	$jokeId = iconv("GBK", "UTF-8", $row['joke_id']);
+	$jokeContent = iconv("GBK", "UTF-8", $row['content']);
+	$createTime = iconv("GBK", "UTF-8", $row['create_time']);
+	$userId = iconv("GBK", "UTF-8", $row['user_id']);
+	$nickName = iconv("GBK", "UTF-8", $row['nick_name']);
+	$avatar = iconv("GBK", "UTF-8", $row['avatar']);
+
+	$jokesArr = array('jokeId' => $jokeId,
+						'jokeContent' => $jokeContent,
+						'createTime' => $createTime,
+						'userId' => $userId,
+						'nickName' => $nickName,
+						'avatar' => $avatar);
+	$jokesList[$i++] = $jokesArr;
+}
+$totalCount = mysql_num_rows($result);
+
+echo json_encode(array('status' => '1', 'totalCount' => $totalCount, 'jokesList' => $jokesList, 'function' => 'getcollectjokes.php'));
+
+mysql_close($con);
+mysql_free_result($result);
+
+?>
